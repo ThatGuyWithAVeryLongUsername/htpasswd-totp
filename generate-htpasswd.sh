7d4d4c53dc429050770e67f471ab45472b998bb3
Pavel Andreev <github2@liaz.us.to>
2023-08-21 12:42:44 +0300

    added comment about dependencies

--- generate-htpasswd.sh
+++ generate-htpasswd.sh
@@ -3,21 +3,21 @@
 # Tested with nginx
 # 
 # USAGE : generate-htpasswd.json /path/to/config/json /path/to/.htpasswd
+# (!!!) REQUIRES htpasswd, jq & oathtool 
 
-cfgFile=$1
-htpasswdFile=$2
-
+# VARIABLES
 # Username used for HTTP AUTH
 user=$(jq -r '.user' $cfgFile)
 # Secret to generate TOTP
 secret=$(jq -r '.secret' $cfgFile)
 
+# READ INPUT
+cfgFile=$1
+htpasswdFile=$2
+
 # Checking that .htpasswd exist. Otherwise htpasswd will ask to use -c flag
 cat $htpasswdFile > /dev/null || touch $htpasswdFile
-
-
-
-# Infinite loop for generating new .htpasswd every 30s 
+# Infinite loop for generating new .htpasswd every 30s
 while :
 do
     htpasswd -b $htpasswdFile $user $(oathtool --totp -s 1800 --base32 $secret)
